103fa8a7d43799490bf5f7de7d20b4b421bcb6d1
felipe217 <felipealvarez487@gmail.com>
2017-06-15 21:53:42 -0600

    Add files via upload

---
+++ MNIST.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+#Inicio fase de entrenamiento
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+#Inicio fase de evaluacion
+imagen = cv2.imread('data/one.jpg', 0)
+temp = np.array(imagen)
+image_binary = np.reshape(temp, (1,784))
+
+result = y
+print(sess.run(result, feed_dict={x:image_binary}))
+
+
+
+
